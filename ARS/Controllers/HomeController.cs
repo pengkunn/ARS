f49be0c607d6d330e98f7f0fa6dabadbfe919138
pk <pengkun.hb@gmail.com>
2015-03-26 01:15:27 +0800

    add

---
+++ ARS/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SQLite;
+using System.Data;
+using System.Data.Linq;
+using ARS.Models;
+
+namespace ARS.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+
+            var strCon = System.Configuration.ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+            SQLiteConnection con = new SQLiteConnection(strCon);
+            //SQLiteCommand cmd = new SQLiteCommand("select * from Employee", con);
+            //SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
+            //DataTable dt = new DataTable();
+            //da.Fill(dt);
+            //var s = dt.Rows[0].ItemArray[0];
+            //dataGridView1.DataSource = dt;
+
+
+            var context = new DataContext(con);
+
+            var employees = context.GetTable<Employee>();
+
+            Employee newEmployee = new Employee() {id=2,password="1",truename="test",username="test" };
+
+            employees.InsertOnSubmit(newEmployee);
+
+            context.SubmitChanges();
+
+            return View();
+        }
+
+    }
+}
---
+++ ARS/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Web;
+
+namespace ARS.Models
+{
+    [Table(Name = "Employee")]
+    public class Employee
+    {
+        [Column(Name = "id",IsPrimaryKey=true)]
+        public int id { get; set; }
+
+        [Column(Name = "username")]
+        public string username { get; set; }
+
+        [Column(Name = "password")]
+        public string password { get; set; }
+
+        [Column(Name = "truename")]
+        public string truename { get; set; }
+
+    }
+}
