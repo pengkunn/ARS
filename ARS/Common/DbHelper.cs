ef4395a1efce7a363835d72b985c864b56a86334
pk <pengkun.hb@gmail.com>
2015-03-27 01:08:54 +0800

    add dbhelper

---
+++ ARS/Common/DbHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Data.SQLite;
+using System.Linq;
+using System.Web;
+
+namespace ARS.Common
+{
+    public class DbHelper
+    {
+        static DbHelper _dbHelper = null;
+
+        static public DbHelper getInstance()
+        {
+            if (_dbHelper == null)
+            {
+                _dbHelper = new DbHelper();
+            }
+
+            return _dbHelper;
+        }
+
+        DataContext _context = null;
+
+        public DbHelper()
+        {
+            var strCon = System.Configuration.ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+            SQLiteConnection con = new SQLiteConnection(strCon);
+            _context = new DataContext(con);
+        }
+
+        public DataContext getDbContext()
+        {
+            return _context;
+        }
+    }
+}
